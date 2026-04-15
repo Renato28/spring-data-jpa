d75a1cb277a0d52ed7da361f4864bbdf4e745948
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-15 16:30:33 -0300

    implementando o repositorio BookRepository

---
+++ src/main/java/br/com/github/renato28/springdatajpa/BookRepository.java
@@ -0,0 +1,10 @@
+package br.com.github.renato28.springdatajpa;
+
+import br.com.github.renato28.springdatajpa.models.BookModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<BookModel, UUID> {
+
+}
