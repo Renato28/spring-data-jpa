0a6d4975eafbf6118ef0d402dcb298f370d1a445
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-15 16:09:16 -0300

    implementando a entidade ReviewModel

---
+++ src/main/java/br/com/github/renato28/springdatajpa/models/ReviewModel.java
@@ -0,0 +1,34 @@
+package br.com.github.renato28.springdatajpa.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_REVIEW")
+public class ReviewModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
