14dcd8c70249cbefaecbe2b8d3d3f3709a0ca41b
Renato Nobrega <renatonobrega87@gmail.com>
2026-04-15 16:06:30 -0300

    implementando a entidade PublisherModel

--- src/main/java/br/com/github/renato28/springdatajpa/models/PublisherModel.java
+++ src/main/java/br/com/github/renato28/springdatajpa/models/PublisherModel.java
@@ -6,15 +6,14 @@ import java.io.Serializable;
 import java.util.UUID;
 
 @Entity
-@Table(name = "TB_AUTHOR")
-public class AuthorModel implements Serializable {
+@Table(name = "TB_PUBLISHER")
+public class PublisherModel implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @Column(nullable = false, unique = true)
     private String name;
 
     public UUID getId() {
